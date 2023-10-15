eabd9c63ee7f400d93d0948143fabf63bf4b592c
Ivan Xavier <ivanxavier@ua.pt>
2023-10-15 20:53:03 +0100

    Performance Tips

--- 02 - Advanced/09 - Performance/02 - Performance/src/script.js
+++ 02 - Advanced/09 - Performance/02 - Performance/src/script.js
@@ -1,6 +1,7 @@
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 import Stats from 'stats.js'
+import * as BufferGeometryUtils from 'three/examples/jsm/utils/BufferGeometryUtils.js'
 
 /**
  * Performance Monitor
@@ -211,21 +212,38 @@ tick()
 // }
 
 // // Tip 19
-// for(let i = 0; i < 50; i++)
-// {
-//     const geometry = new THREE.BoxGeometry(0.5, 0.5, 0.5)
+/*
+const geometries = []
 
-//     const material = new THREE.MeshNormalMaterial()
-    
-//     const mesh = new THREE.Mesh(geometry, material)
-//     mesh.position.x = (Math.random() - 0.5) * 10
-//     mesh.position.y = (Math.random() - 0.5) * 10
-//     mesh.position.z = (Math.random() - 0.5) * 10
-//     mesh.rotation.x = (Math.random() - 0.5) * Math.PI * 2
-//     mesh.rotation.y = (Math.random() - 0.5) * Math.PI * 2
+for(let i = 0; i < 50; i++)
+{
+    const geometry = new THREE.BoxGeometry(0.5, 0.5, 0.5)
 
-//     scene.add(mesh)
-// }
+    geometry.translate(
+        (Math.random() - 0.5) * 10,
+        (Math.random() - 0.5) * 10,
+        (Math.random() - 0.5) * 10,
+    )
+
+    geometry.rotateX((Math.random() - 0.5) * Math.PI * 2)
+    geometry.rotateY((Math.random() - 0.5) * Math.PI * 2)
+
+    geometries.push(geometry)
+}
+
+const mergedGeometries = BufferGeometryUtils.mergeGeometries(geometries)
+
+const material = new THREE.MeshNormalMaterial()
+
+const mesh = new THREE.Mesh(mergedGeometries, material)
+mesh.position.x = (Math.random() - 0.5) * 10
+mesh.position.y = (Math.random() - 0.5) * 10
+mesh.position.z = (Math.random() - 0.5) * 10
+mesh.rotation.x = (Math.random() - 0.5) * Math.PI * 2
+mesh.rotation.y = (Math.random() - 0.5) * Math.PI * 2
+
+scene.add(mesh)
+*/
 
 // // Tip 20
 // const geometry = new THREE.BoxGeometry(0.5, 0.5, 0.5)
@@ -245,82 +263,118 @@ tick()
 // }
 
 // // Tip 22
-// const geometry = new THREE.BoxGeometry(0.5, 0.5, 0.5)
+/*
+const geometry = new THREE.BoxGeometry(0.5, 0.5, 0.5)
 
-// const material = new THREE.MeshNormalMaterial()
-    
-// for(let i = 0; i < 50; i++)
-// {
-//     const mesh = new THREE.Mesh(geometry, material)
-//     mesh.position.x = (Math.random() - 0.5) * 10
-//     mesh.position.y = (Math.random() - 0.5) * 10
-//     mesh.position.z = (Math.random() - 0.5) * 10
-//     mesh.rotation.x = (Math.random() - 0.5) * Math.PI * 2
-//     mesh.rotation.y = (Math.random() - 0.5) * Math.PI * 2
+const material = new THREE.MeshNormalMaterial()
 
-//     scene.add(mesh)
-// }
+const mesh = new THREE.InstancedMesh(geometry, material, 50)
+// Animate need dynamic matrix, activate this if needed in the tick()
+mesh.instanceMatrix.setUsage(THREE.DynamicDrawUsage)
+scene.add(mesh)
+
+for(let i = 0; i < 50; i++)
+{
+    const position = new THREE.Vector3(
+    (Math.random() - 0.5) * 10,
+    (Math.random() - 0.5) * 10,
+    (Math.random() - 0.5) * 10,
+    )
+
+    const quaternion = new THREE.Quaternion()
+    quaternion.setFromEuler(new THREE.Euler(
+    (Math.random() - 0.5) * Math.PI * 2,
+    (Math.random() - 0.5) * Math.PI * 2
+    ))
+
+    const matrix = new THREE.Matrix4()
+    matrix.makeRotationFromQuaternion(quaternion)
+    matrix.setPosition(position)
+    mesh.setMatrixAt(i, matrix)
+
+    scene.add(mesh)
+}
+*/
 
 // // Tip 29
 // renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 
 // // Tip 31, 32, 34 and 35
-// const shaderGeometry = new THREE.PlaneGeometry(10, 10, 256, 256)
+const shaderGeometry = new THREE.PlaneGeometry(10, 10, 256, 256)
 
-// const shaderMaterial = new THREE.ShaderMaterial({
-//     uniforms:
-//     {
-//         uDisplacementTexture: { value: displacementTexture },
-//         uDisplacementStrength: { value: 1.5 }
-//     },
-//     vertexShader: `
-//         uniform sampler2D uDisplacementTexture;
-//         uniform float uDisplacementStrength;
+const shaderMaterial = new THREE.ShaderMaterial({
+    uniforms:
+    {
+        defines:    // Alternative for defining static values
+        {
+            uDisplacementStrength: 0.5,
+        },
+        precision: 'lowp',  //
+        uDisplacementTexture: { value: displacementTexture },
+        uDisplacementStrength: { value: 1.5 }
+    },
+    vertexShader: `
+        uniform sampler2D uDisplacementTexture;
+        //uniform float uDisplacementStrength;
+        #define uDisplacementStrength 1.5   // Static value
 
-//         varying vec2 vUv;
+        varying vec2 vUv;
+        varying vec3 vFinalColor;
 
-//         void main()
-//         {
-//             vec4 modelPosition = modelMatrix * vec4(position, 1.0);
+        void main()
+        {
+            // Position
+            vec4 modelPosition = modelMatrix * vec4(position, 1.0);
 
-//             float elevation = texture2D(uDisplacementTexture, uv).r;
-//             if(elevation < 0.5)
-//             {
-//                 elevation = 0.5;
-//             }
+            float elevation = texture2D(uDisplacementTexture, uv).r;
+            //if(elevation < 0.5)
+            //{
+            //    elevation = 0.5;
+            //}
+            modelPosition.y = clamp(elevation, 0.5, 1.0) * uDisplacementStrength;
 
-//             modelPosition.y += elevation * uDisplacementStrength;
+            modelPosition.y += elevation * uDisplacementStrength;
 
-//             gl_Position = projectionMatrix * viewMatrix * modelPosition;
+            gl_Position = projectionMatrix * viewMatrix * modelPosition;
 
-//             vUv = uv;
-//         }
-//     `,
-//     fragmentShader: `
-//         uniform sampler2D uDisplacementTexture;
+            // Color
+            float colorElevation = max(elevation, 0.25);
+            vec3 depthColor = vec3(1.0, 0.1, 0.1);
+            vec3 surfaceColor = vec3(0.1, 0.0, 0.5);
+            vec3 finalColor = mix(depthColor, surfaceColor, colorElevation);
 
-//         varying vec2 vUv;
+            vUv = uv;
+            vFinalColor = finalColor;
+        }
+    `,
+    fragmentShader: `
+        uniform sampler2D uDisplacementTexture;
 
-//         void main()
-//         {
-//             float elevation = texture2D(uDisplacementTexture, vUv).r;
-//             if(elevation < 0.25)
-//             {
-//                 elevation = 0.25;
-//             }
+        varying vec2 vUv;
+        varying vec3 vFinalColor;
 
-//             vec3 depthColor = vec3(1.0, 0.1, 0.1);
-//             vec3 surfaceColor = vec3(0.1, 0.0, 0.5);
-//             vec3 finalColor = vec3(0.0);
-//             finalColor.r += depthColor.r + (surfaceColor.r - depthColor.r) * elevation;
-//             finalColor.g += depthColor.g + (surfaceColor.g - depthColor.g) * elevation;
-//             finalColor.b += depthColor.b + (surfaceColor.b - depthColor.b) * elevation;
+        void main()
+        {
+            //float elevation = texture2D(uDisplacementTexture, vUv).r;
+            //if(elevation < 0.25)
+            //{
+            //    elevation = 0.25;
+            //}
 
-//             gl_FragColor = vec4(finalColor, 1.0);
-//         }
-//     `
-// })
+            //vec3 depthColor = vec3(1.0, 0.1, 0.1);
+            //vec3 surfaceColor = vec3(0.1, 0.0, 0.5);
+            //vec3 finalColor = vec3(0.0);
+            //finalColor.r += depthColor.r + (surfaceColor.r - depthColor.r) * elevation;
+            //finalColor.g += depthColor.g + (surfaceColor.g - depthColor.g) * elevation;
+            //finalColor.b += depthColor.b + (surfaceColor.b - depthColor.b) * elevation;
 
-// const shaderMesh = new THREE.Mesh(shaderGeometry, shaderMaterial)
-// shaderMesh.rotation.x = - Math.PI * 0.5
-// scene.add(shaderMesh)
+            //vec3 finalColor = mix(depthColor, surfaceColor, elevation);
+
+            gl_FragColor = vec4(vFinalColor, 1.0);
+        }
+    `
+})
+
+const shaderMesh = new THREE.Mesh(shaderGeometry, shaderMaterial)
+shaderMesh.rotation.x = - Math.PI * 0.5
+scene.add(shaderMesh)
